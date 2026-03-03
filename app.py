d2e85273e08d4be3515053397cbe3bd4b6cd0232
David-Chan-Ho2 <dho12@charlotte.edu>
2026-03-03 14:54:55 -0500

    test ci/cd again

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import shutil
 import uuid
-# Testing CI/CD pipeline
+# Testing CI/CD pipeline 2
 from dotenv import load_dotenv
 
 # Load env vars (e.g., GROQ_API_KEY) from project .env reliably.
